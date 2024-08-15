1d658637f347c9508d15abcb4483d69d5f89e0d7
Merlin Migan <slavikzaya@gmail.com>
2024-08-15 12:48:15 +0200

    Ajout de commentaires pour annoter les middlewares de gestion d'erreurs !

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ const orderRoutes = require('./api/routes/orders');
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-// middleware handler 1/2
+// middleware error handler 1/2
 app.use((req, res, next) => {
    const error =  new Error('Not found');
    error.status = 404;
    next(error);
 });
 
-// middleware handler 2/2
+// middleware error handler 2/2
 app.use((error, req, res, next) => {
    res.status(error.status || 500);
    res.json({
       error: {
-         message: error.message
+         message: error.message,
       }
    });
 });
